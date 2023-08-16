f8c5618591d5f5b6a2b7b85856d5c87706e580fc
Patricia Bertran <pbertranc@gmail.com>
2023-08-16 16:46:32 +0200

    simple airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,25 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
+  end
+end
